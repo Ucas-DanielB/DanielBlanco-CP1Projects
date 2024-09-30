dac1b35db94a565552c764b9a9f2aceb3fc4f74e
EthanBlanco25 <EthanBlanco15>
2024-09-29 21:38:37 -0600

    Ethans personal information converter

---
+++ Ethan converter/main.py
@@ -0,0 +1,12 @@
+#Ethan Blanco, ProfinciecyTest: Personal Information Converter
+
+name = input("What is your name: ")
+age = input("How old are you: ")
+height = input ("How tall are you measured in meters: ")
+FavNum = input("What is your favorite number: ")
+
+converted_age = int(age)
+converted_FavNum = int(FavNum)
+converted_height = float(height)
+
+print(name, age, height, FavNum, converted_age,converted_FavNum,converted_height)
